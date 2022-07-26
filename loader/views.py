a34ba2bffbd7eeb3b1c4ce7a43dd90104b4b02db
Wildtsk <wildtsk@gmail.com>
2022-07-26 12:15:42 +0300

    "добавление logging"

--- loader/views.py
+++ loader/views.py
@@ -1,3 +1,6 @@
+import logging
+from json import JSONDecodeError
+
 from flask import Blueprint, render_template, request
 
 from hw_12_.functions import add_post
@@ -19,6 +22,15 @@ def add_post_page():
     if not picture or not content:
         return 'Нет картинки или текста'
 
-    picture_path: str = '/' + save_picture(picture)
+    if picture.filename.split('.')[-1] not in ['jpeg', 'png']:
+        logging.info('Загруженный файл не является картинкой')
+        return 'Не тот формат файла, нужен jpeg или png'
+    try:
+        picture_path: str = '/' + save_picture(picture)
+    except FileNotFoundError:
+        logging.error('файл не найден')
+        return 'Возможно здесь ошибка, файл не найден'
+    except JSONDecodeError:
+        return 'Что то с файлом не так'
     post: dict = add_post({'pic': picture_path, 'content': content})
     return render_template('post_uploaded.html', post=post)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
+import logging
+from json import JSONDecodeError
+
 from flask import Blueprint, render_template, request
 
 from hw_12_.functions import get_posts_by_word
@@ -12,5 +15,12 @@ def mane_page():
 @main_blueprint.route("/search/")
 def search_page():
     search_query = request.args.get("s", "")
-    posts = get_posts_by_word(search_query)
+    logging.info('Поиск выполняется')
+    try:
+        posts = get_posts_by_word(search_query)
+    except FileNotFoundError:
+        logging.error('файл не найден')
+        return 'Что то пошло не так.. Возможно файла не существует?'
+    except JSONDecodeError:
+        return 'Что то с файлом не так'
     return render_template("post_list.html", query=search_query, posts=posts)
